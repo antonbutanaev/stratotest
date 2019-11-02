d88e5ecd2ae797648e72b4c4a61fb304bc0c6bf5
Антон <antonbutanaev@gmail.com>
2019-11-02 20:46:01 +0700

    Added date

--- main.cpp
+++ main.cpp
@@ -1,4 +1,17 @@
 #include <iostream>
+#include <chrono>
+#include <vector>
+#include <date/date.h>
+
+using Time = std::chrono::time_point<std::chrono::system_clock, std::chrono::minutes>;
+using Price = double;
+
+struct Quote {
+	Time time;
+	Price price;
+};
+
+using Quotes = std::vector<Quote>;
 
 int main() {
 }
