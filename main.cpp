2cc22bfda1aa38c483120d15df2bca54c84c6022
Anton Butanaev <abutanaev@ac-sw.com>
2019-12-09 10:55:46 +0700

    Удаление стратегии SGCapital

--- main.cpp
+++ main.cpp
@@ -15,15 +15,6 @@
 using namespace std;
 using namespace date;
 
-struct Strategy {
-	struct Module {
-		std::vector<Ticker> instruments;
-		double weight;
-	};
-	using Modules = std::vector<Module>;
-	Modules modules;
-};
-
 using  Portfolio = std::map<Ticker, size_t>;
 
 void buy(const Ticker &ticker, Price sum, const Date &date, Price &cash, Portfolio &portfolio) {
@@ -31,45 +22,6 @@ void buy(const Ticker &ticker, Price sum, const Date &date, Price &cash, Portfol
 	size_t num = sum / price;
 	portfolio[ticker] += num;
 	cash -= num * price;
-}
-
-void buy(const Strategy &strategy, const Date &date, Price &cash, Portfolio &portfolio) {
-	const auto prevDate = date - months{6};
-	const auto cashOrig = cash;
-	for (const auto &module: strategy.modules) {
-		double maxChange = 0;
-		Price price = NoPrice;
-		decltype(module.instruments.begin()) bestInstrumentIt;
-		for (auto instrumentIt = module.instruments.begin(); instrumentIt != module.instruments.end(); ++instrumentIt) {
-			const auto nowPrice = Quotes::get().getQuote(*instrumentIt, date);
-			const auto prevPrice = Quotes::get().getQuote(*instrumentIt, prevDate);
-			const auto change = nowPrice/prevPrice - 1;
-			LOG0(
-				"Performance of " << *instrumentIt
-				<< " from " << prevDate << " to " << date
-				<< " is " << prevPrice << " - " << nowPrice
-				<< " change " << change
-			);
-
-			if (change >= 0 && maxChange < change) {
-				maxChange = change;
-				price = nowPrice;
-				bestInstrumentIt = instrumentIt;
-			}
-		}
-
-		if (price != NoPrice) {
-			unsigned num = cashOrig * module.weight / price;
-			LOG(
-				"Best instrument in module " << (&module - &strategy.modules.front())
-				<< " is " << *bestInstrumentIt
-				<< " weight " << module.weight
-				<< " BUY on " << date << " num " << num << " for " << price
-			);
-			cash -= num * price;
-			portfolio[*bestInstrumentIt] += num;
-		}
-	}
 }
 
 void sellAll(const Date &date, Price &cash, Portfolio &portfolio) {
@@ -84,74 +36,6 @@ void sellAll(const Date &date, Price &cash, Portfolio &portfolio) {
 
 	LOG("CASH on " << date << " " << cash << " draw down " << (100. * (cash / maxCash - 1.)) << "%");
 }
-
-void testStrategy(const Strategy &strategy, const Date &startDate) {
-	const auto endDate = year{2019}/10/1;
-	Price cash = 10000.;
-	auto cashOrig = cash;
-	Portfolio portfolio;
-	CashAnalyzer cashAnalyzer;
-	const auto printPortfolio = [&](const auto &date){
-		for (const auto &position: portfolio) {
-			LOG("ON " << date << " TICKER " << position.first << " " << position.second);
-		}
-	};
-	buy(strategy, startDate, cash, portfolio);
-	for (auto date = startDate + months{1}; date < endDate; date += months{1}) {
-		printPortfolio(date);
-		sellAll(date, cash, portfolio);
-		cashAnalyzer.addBalance(cash);
-		buy(strategy, date, cash, portfolio);
-	}
-	sellAll(endDate, cash, portfolio);
-	cashAnalyzer.addBalance(cash);
-	cashAnalyzer.result(startDate, endDate, cash, cashOrig);
-}
-
-void strategySGCapital() {
-	const Strategy strategy {
-		{
-			{
-				{
-					"VTI",
-					"VEA",
-					"VWO",
-					"SHV",
-				},
-				0.35,
-			},
-			Strategy::Module{
-				{
-					"LQD",
-					"TLT",
-					"EMB",
-					"SHV",
-				},
-				0.35
-
-			},
-			Strategy::Module{
-				{
-					"IAU",
-					"DBC",
-					"VNQ",
-					"SHV",
-				},
-				0.3,
-			},
-		}
-	};
-
-	for (const auto &module: strategy.modules)
-		for (const auto &instrument: module.instruments)
-			Quotes::get().parseQuotes(instrument);
-
-	cout << fixed << setprecision(2);
-
-	for (auto date = year{2008}/7/1; date < year{2018}/10/1; date += months{1})
-		testStrategy(strategy, date);
-}
-
 
 using Tickers = vector<Ticker>;
 
