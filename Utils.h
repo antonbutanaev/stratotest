0a4099ce462ddd9549cc8c5d85ecd6071a8a88c8
Anton Butanaev <abutanaev@ac-sw.com>
2019-12-19 11:39:22 +0700

    Доводка Шарпа

--- Utils.h
+++ Utils.h
@@ -5,21 +5,18 @@
 #include <cmath>
 
 template<class T>
-double sharpe(const T &gaines, const double risklessGain = 0.) {
+double sharpe(const T &gaines, const double risklessGain = .02/12) {
 	const auto n = std::distance(std::begin(gaines), std::end(gaines));
 	if (n == 0)
 		return 0;
 
 	const auto avg = std::accumulate(
-		std::begin(gaines), std::end(gaines), 0., [&] (auto cum, auto gain) {
-			return cum + gain;
-		}
-	) / n;
+		std::begin(gaines), std::end(gaines), 0.) / n;
 
 	const auto dev = sqrt(
 		std::accumulate(
 			std::begin(gaines), std::end(gaines), 0., [&](auto cum, auto gain){
-				const auto delta = gain - risklessGain;
+				const auto delta = gain - risklessGain - avg;
 				return cum + delta * delta;
 		}) / n
 	);
